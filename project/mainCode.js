48e38133458f40e08871f4fb54dab844be653618
Xermax <Xermax3@users.noreply.github.com>
2020-12-14 19:56:54 -0500

    Set up files and folders - Ready to begin

---
+++ project/mainCode.js
@@ -0,0 +1,19 @@
+/*
+    TITLE HERE
+    Created by: Maxence Roy
+    Latest update: December 12, 2020
+    Description: DESCRIPTION HERE
+*/
+
+/*
+    To do:
+    - Array manipulation (add/remove objects)
+    - Movement / Animation loop
+    - Scoring
+    - Game end condition
+    - Limit checking
+    - Sounds
+    - Intro splash screen (w/ instructions)
+    - Game over / Play again screen
+    - Originality
+*/
